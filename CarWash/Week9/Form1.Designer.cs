d23f7031c343394ebfe5049f50a6d4a9d49b532c
darkawesome <38367397+darkawesome@users.noreply.github.com>
2021-10-17 06:49:44 -0400

    Add files via upload

---
+++ CarWash/Week9/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace Week9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkNav = new System.Windows.Forms.CheckBox();
+            this.chkLeather = new System.Windows.Forms.CheckBox();
+            this.chkStereo = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdbCustom = new System.Windows.Forms.RadioButton();
+            this.rdbPearl = new System.Windows.Forms.RadioButton();
+            this.rdbStd = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.btClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkNav);
+            this.groupBox1.Controls.Add(this.chkLeather);
+            this.groupBox1.Controls.Add(this.chkStereo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(282, 222);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Accessories ";
+            // 
+            // chkNav
+            // 
+            this.chkNav.AutoSize = true;
+            this.chkNav.Location = new System.Drawing.Point(17, 152);
+            this.chkNav.Name = "chkNav";
+            this.chkNav.Size = new System.Drawing.Size(187, 24);
+            this.chkNav.TabIndex = 2;
+            this.chkNav.Text = "Navigation ($1,741.23)";
+            this.chkNav.UseVisualStyleBackColor = true;
+            // 
+            // chkLeather
+            // 
+            this.chkLeather.AutoSize = true;
+            this.chkLeather.Location = new System.Drawing.Point(17, 94);
+            this.chkLeather.Name = "chkLeather";
+            this.chkLeather.Size = new System.Drawing.Size(209, 24);
+            this.chkLeather.TabIndex = 1;
+            this.chkLeather.Text = "Leather Interior ($987.41)";
+            this.chkLeather.UseVisualStyleBackColor = true;
+            // 
+            // chkStereo
+            // 
+            this.chkStereo.AutoSize = true;
+            this.chkStereo.Location = new System.Drawing.Point(17, 41);
+            this.chkStereo.Name = "chkStereo";
+            this.chkStereo.Size = new System.Drawing.Size(205, 24);
+            this.chkStereo.TabIndex = 0;
+            this.chkStereo.Text = "Stereo System ($425.76)";
+            this.chkStereo.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdbCustom);
+            this.groupBox2.Controls.Add(this.rdbPearl);
+            this.groupBox2.Controls.Add(this.rdbStd);
+            this.groupBox2.Location = new System.Drawing.Point(12, 240);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(282, 198);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Exterior Finish";
+            // 
+            // rdbCustom
+            // 
+            this.rdbCustom.AutoSize = true;
+            this.rdbCustom.Location = new System.Drawing.Point(17, 149);
+            this.rdbCustom.Name = "rdbCustom";
+            this.rdbCustom.Size = new System.Drawing.Size(249, 24);
+            this.rdbCustom.TabIndex = 2;
+            this.rdbCustom.TabStop = true;
+            this.rdbCustom.Text = "Customized Detailing ($599.99)";
+            this.rdbCustom.UseVisualStyleBackColor = true;
+            // 
+            // rdbPearl
+            // 
+            this.rdbPearl.AutoSize = true;
+            this.rdbPearl.Location = new System.Drawing.Point(17, 95);
+            this.rdbPearl.Name = "rdbPearl";
+            this.rdbPearl.Size = new System.Drawing.Size(164, 24);
+            this.rdbPearl.TabIndex = 1;
+            this.rdbPearl.TabStop = true;
+            this.rdbPearl.Text = "Pearlized ($345.72)";
+            this.rdbPearl.UseVisualStyleBackColor = true;
+            // 
+            // rdbStd
+            // 
+            this.rdbStd.AutoSize = true;
+            this.rdbStd.Location = new System.Drawing.Point(17, 38);
+            this.rdbStd.Name = "rdbStd";
+            this.rdbStd.Size = new System.Drawing.Size(178, 24);
+            this.rdbStd.TabIndex = 0;
+            this.rdbStd.TabStop = true;
+            this.rdbStd.Text = "Standard (no charge)";
+            this.rdbStd.UseVisualStyleBackColor = true;
+            this.rdbStd.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(473, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Car Sales Price :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(473, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Accesories &  Finish :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(473, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Subtotal :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(473, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Sales Tax (8%) :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(473, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Total :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(473, 282);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Trade-in Allowance:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(473, 329);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Amount Due:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(630, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 26);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(630, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 26);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(630, 135);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 26);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(630, 184);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(122, 26);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(630, 230);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(122, 26);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(630, 276);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(122, 26);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(630, 323);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(122, 26);
+            this.textBox7.TabIndex = 15;
+            // 
+            // btnCal
+            // 
+            this.btnCal.Location = new System.Drawing.Point(414, 403);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(93, 31);
+            this.btnCal.TabIndex = 16;
+            this.btnCal.Text = "Calculate";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // btClear
+            // 
+            this.btClear.Location = new System.Drawing.Point(551, 398);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(88, 36);
+            this.btClear.TabIndex = 17;
+            this.btClear.Text = "Clear";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(670, 403);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 36);
+            this.btnExit.TabIndex = 18;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Christophers Car center";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkNav;
+        private System.Windows.Forms.CheckBox chkLeather;
+        private System.Windows.Forms.CheckBox chkStereo;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdbCustom;
+        private System.Windows.Forms.RadioButton rdbPearl;
+        private System.Windows.Forms.RadioButton rdbStd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ CarWash/Week9/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            double dbldue = 0;
+            double dblAllow = 2000;
+            double dblPrice = 0;
+            double dblExtras = 0;
+            double dblTotal = 0;
+            double dblSub = 0;
+            double dblTax = 0;
+            double dblAccess = 0;
+
+            if (chkLeather.Checked)
+            {
+                dblPrice += 987.41;
+            }
+            else if (chkNav.Checked) 
+            {
+                dblPrice += 1741.23;
+            }
+            else if (chkStereo.Checked)
+            {
+                dblPrice += 425.76;
+            }
+            if(rdbCustom.Checked)
+            {
+                dblExtras += 599.99;
+            }
+            else if(rdbPearl.Checked)
+            {
+                dblExtras += 345.72;
+            }
+            else if (rdbStd.Checked)
+            {
+                dblExtras += 0;
+            }
+
+            dblAccess = (dblPrice + dblExtras);
+            dblSub = (dblPrice+dblExtras + 20000);
+            dblTax = (dblSub * 0.08);
+            dblTotal = (dblSub + dblTax);
+            dbldue = (dblTotal - dblAllow);
+
+            textBox1.Text = "20000";
+            textBox2.Text =string.Format ("{0}", dblAccess);
+            textBox3.Text =string.Format("{0}", dblSub);
+            textBox4.Text = string.Format("{0}", dblTax);
+            textBox5.Text = string.Format("{0}", dblTotal);
+            textBox6.Text = string.Format("{0}", dblAllow);
+            textBox7.Text = string.Format("{0}", dbldue);
+
+
+
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+        }
+    }
+}
